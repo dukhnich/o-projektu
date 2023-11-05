0c49c32587d514301d01056ea040b15b7bf92e4e
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-05 18:34:50 +0100

    add text about project

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -11,7 +11,15 @@ export const HomePage = () => {
       <Container>
       <main>
         <h1>Sluneční Průvodce</h1>
-        <p>O projektu</p>
+        <p>
+          Cílem našeho projektu je informovat a edukovat lidi o vlivu slunečního záření na kůži a nabídnout jim konkrétní doporučení pro ochranu kůže. 
+        </p>
+        <p>
+          Chceme vytvořit moderní a uživatelsky přívětivou webovou stránku, kde každý návštěvník má možnost vyplnit test na určení fototypu své kůže a získat doporučení pro konkrétní ochranu. Taky planujeme přidat UV mapu, která poskytuje důležité informace o aktuálním UV indexu a umožňuje lidem plánovat svůj čas venku tak, aby minimalizovali riziko poškození kůže.
+        </p>
+        <p>
+          Doufáme, že tento projekt bude pro nás skvělou příležitostí vyzkoušet a zdokonalit naše dovednosti v oblasti frontend vývoje.
+        </p>
       </main>
       </Container>
       <Footer />
