4297f3ebf5b372f10868bb11220d1410f23fccb0
Chuiko Anastasiia <nastyachuyko33@gmail.com>
2023-11-12 14:13:43 +0200

    3 stranka

--- src/pages/Chuiko/index.jsx
+++ src/pages/Chuiko/index.jsx
@@ -1,14 +1,34 @@
 import { Link } from 'react-router-dom'; 
 import './style.css';
 import ChuikoImg from './photobakery-95.jpg'
+import { Container } from 'react-bootstrap';
+
 
 export const Chuiko = () => {
   return (
-    <div className="container">
+    
+    <Container className='py-4'>
+        <img src={ChuikoImg} alt="Anastasiia Chuiko"></img>
+   <header>
+        <h2 className='text-secondary mb-4'>Anastasiia Chuiko</h2>
       <p>
       Ještě před přestěhováním do České republiky jsem chtěla změnit své povolání, najít si pro sebe stabilní zaměstnání, které by mi přinášelo potěšení a stabilitu bez ohledu na to, kde jsem a co se děje ve světě. A tak jsem po přestěhování objevila svět IT a rozhodla se v něm rozvíjet po malých krůčcích.
       </p>
-    <img src={ChuikoImg} alt="Anastasiia Chuiko"></img>
-    </div>
+      <p>
+      Z projektu chci získat nové znalosti, zkušenosti s prací na reálném projektu, který bude moderní a užitečný pro uživatele.
+      </p>
+      
+      </header>
+      
+    </Container>
+    
   );
 };
+
+{/* <Container className='py-4'>
+<Row className='align-items-center'>
+  <Col xs={6} md={3}>
+    <Image src={photo} alt="Maryna Dukhnich" roundedCircle />
+  </Col>
+  <Col xs={6} md={8}>
+    <h2 className='text-secondary mb-4'>Maryna Dukhnich</h2> */}
