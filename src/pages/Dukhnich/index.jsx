c11b969dfb22a440ecd610ba6471acbc51ab5c6d
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-12 17:40:31 +0100

    add personal limks

--- src/pages/Dukhnich/index.jsx
+++ src/pages/Dukhnich/index.jsx
@@ -5,16 +5,17 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 export const Dukhnich = () => {
   return (
     <Container className='py-4'>
-      <Row className='align-items-center'>
-        <Col xs={6} md={3}>
-          <Image src={photo} alt="Maryna Dukhnich" roundedCircle />
+      <Row>
+        <Col xs={12} sm={6} md={3}>
+          <Image src={photo} alt="Maryna Dukhnich" roundedCircle className="mb-4" />
         </Col>
-        <Col xs={6} md={8}>
-          <h2 className='text-secondary mb-4'>Maryna Dukhnich</h2>
+        <Col xs={12} sm={6} md={8}>
+          <h1 className='text-muted mt-0'>Maryna Dukhnich</h1>
           <p>
             Ahoj, jsem Maryna Dukhnich a ráda bych vám představila sebe jako účastnice tohoto projektu.
           </p>
@@ -24,6 +25,8 @@ export const Dukhnich = () => {
           <p>
             Doufám, že vás naše webová stránka osloví a poskytne vám užitečné informace.
           </p>
+          <Button variant="primary" className="mt-4" href="https://github.com/dukhnich">Github</Button>
+          <Button variant="primary" className="mt-4 mx-2" href="https://www.linkedin.com/in/maryna-dukhnich/">LinkedIn</Button>
         </Col>
       </Row>
     </Container>
